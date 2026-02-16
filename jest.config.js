b54efb7f95b501eca1371f070e6e90cc72dfd49a
Lazar Milovanovic <007lazar@gmail.com>
2026-02-16 13:27:50 +0100

    test(jest): add Jest testing framework setup
    Enables CI integration and automated test execution.

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.model.ts',
+    '!src/main.ts',
+    '!src/polyfills.ts',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+};
